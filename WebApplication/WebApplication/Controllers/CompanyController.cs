f27508b89816cc40483a9203a1cc5fd1475089de
benTranCodes <ben.tran@soulfx.com>
2019-02-05 16:16:15 -0500

    ✨Connect database and output list of companies

---
+++ WebApplication/WebApplication/Controllers/CompanyController.cs
@@ -0,0 +1,35 @@
+﻿using WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+
+namespace WebApplication.Controllers
+{
+    [RoutePrefix("company")]
+    public class CompanyController : ApiController
+    {
+
+        [Route]
+        [HttpGet]
+        public IHttpActionResult GetAllCompanies()
+        {
+            var db = new mytestdbEntities();
+
+            var yada = db.Companies.Where(xx => true);
+
+            foreach(var item in yada)
+            {
+                Debug.WriteLine(item.Name);
+            }
+
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, yada));
+        }
+
+    }
+
+
+}
---
+++ WebApplication/WebApplication/Models/CompanyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class CompanyModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int IsActive { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int NumberOfEmployees { get; set; }
+    }
+}
